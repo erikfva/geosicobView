888d5fcdde9491b03565c18b303bb723bdbcb226
Erik Vargas Agreda <erik.vargas@yandex.com>
2017-05-31 15:19:45 -0400

    Actualizados enlaces para cargar librerias desde github

--- src/mapas_base/mapas_base.js
+++ src/mapas_base/mapas_base.js
@@ -1,12 +1,3 @@
-	/*
-  fetch('../build/mapas_base.html').then((resp) => resp.text())
-.then(data => {
-  var div = document.body.appendChild(document.createElement('div'));
-  div.style.display = 'none';
-  div.innerHTML = data;
-  //console.log(data);
-});
-*/
 function renderMapasBase(idcontainer){
 
  if (typeof window.jsonLayers !== 'undefined') return;
@@ -30,10 +21,13 @@ function renderMapasBase(idcontainer){
   document.body.removeChild(span);
 })();
 
-
+		var cloudURL = 'https://erikfva.github.io/geosicobView/';
     $.when(
-    	$.getJSON('http://anyorigin.com/go?url=http%3A//geosicob.bitballoon.com/mapas_base.min.html&callback=?', function(data){
-    		$(data.contents).filter('script').each(function(i,ss){
+     	$.get(cloudURL + 'build/mapas_base.min.css', function(data){
+    		$('<style/>').text(data).appendTo('head');
+    	}),
+    	$.get(cloudURL + 'build/mapas_base.min.html', function(data){
+    		$(data).filter('script').each(function(i,ss){
     			var s = document.createElement('script');
     			s.type = $(ss).attr('type') || 'text/javascript';
     			s.id = $(ss).attr('id') || '';
@@ -41,7 +35,7 @@ function renderMapasBase(idcontainer){
     			$("body").append(s);
     		});
     	}),
-    	$.get('http://res.cloudinary.com/erikvargas/raw/upload/v1495635822/geosicob/base-maps.json',{ dataType: "json" },function(data){
+    	$.getJSON(cloudURL + 'base-maps.json', function( data ) {
     		window.jsonLayers = data.layers;
     	}),
     	typeof window['tmpl'] === 'function'?$.noop():$.get('https://cdn.rawgit.com/blueimp/JavaScript-Templates/master/js/tmpl.min.js',function(ss){
@@ -49,11 +43,11 @@ function renderMapasBase(idcontainer){
     		s.type = 'text/javascript';
     		s.text = ss;
     		$("body").append(s);
-    	}),
-    	$.getJSON('http://anyorigin.com/go?url=http%3A//geosicob.bitballoon.com/mapas_base.min.css&callback=?', function(data){
-    		$('<style/>').text(data.contents).appendTo('head');
     	})
-    ).then(function(d,e){
-    		renderBaseMapsPanel(e[0].layers,idcontainer);
-    });
+    ).then(function(){
+    		renderBaseMapsPanel(jsonLayers,idcontainer);
+    },
+    function (e) {
+    	console.log(e, 'an error occurred somewhere');
+  	});
 }
